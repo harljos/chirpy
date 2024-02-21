8efc26f72a596b23c4ffabc54dcc5c3d23e939eb
unknown <joseph.harley02@gmail.com>
2024-02-21 17:47:01 -0500

    started server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/harljos/chirpy
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+
+	s := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(s.ListenAndServe())
+}
