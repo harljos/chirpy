95b77421a0774b0e85f580a3f9c9b2fc368c9d79
unknown <joseph.harley02@gmail.com>
2024-02-27 13:23:41 -0500

    added admin router for metrics to display html

--- headers.go
+++ headers.go
@@ -16,10 +16,3 @@ func middlewareCors(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits++
-		next.ServeHTTP(w, r)
-	})
-}
